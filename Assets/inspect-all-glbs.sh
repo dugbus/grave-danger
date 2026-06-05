aa16791d39e27cb7cbaebf564ae55cd9dd83608b
Idris Elliott <idrise@davidjelliott.com>
2026-06-06 00:54:00 +0100

    refactored code into folders.

--- Assets/inspect-all-glbs.sh
+++ Assets/inspect-all-glbs.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-
+mkdir -p ../docs
 find . -type f \( -iname "*.glb" -o -iname "*.gltf" \) | while read -r file; do
-    outfile="../docs/${file}.md"
+    filename=$(basename "$file")
+    outfile="../docs/${filename}.md"
 
     echo "Processing: $file"
     gltf-transform inspect "$file" --format=md > "$outfile" 2>&1
