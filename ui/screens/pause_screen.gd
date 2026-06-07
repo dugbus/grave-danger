43fae7a7a554422e10e09e719147791e848a2fba
Idris Elliott <idrise@davidjelliott.com>
2026-06-07 17:09:15 +0100

    Pause mode added. Keyboard 'p' or 'Start' on the controller. It's a 2d scene so we can add UI to it later on

---
+++ ui/screens/pause_screen.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	visible = false
+	set_process_unhandled_input(true)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause_game"):
+		_set_paused(not get_tree().paused)
+		get_viewport().set_input_as_handled()
+
+
+func _exit_tree() -> void:
+	if visible and get_tree().paused:
+		get_tree().paused = false
+
+
+func _set_paused(paused: bool) -> void:
+	get_tree().paused = paused
+	visible = paused
